21b45f2f631922884343fb4e8289abff5a7e32b9
Mario Garcia <mario.ggar@gmail.com>
2023-03-24 08:02:27 +0100

    fix: kafka docker configuration and added log4j2 conf

--- modules/kafka/src/main/java/lab/kafka/api/numbers/NumbersProducer.java
+++ modules/kafka/src/main/java/lab/kafka/api/numbers/NumbersProducer.java
@@ -3,11 +3,14 @@ package lab.kafka.api.numbers;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.util.Properties;
 
 public class NumbersProducer {
+
+    private static final Logger logger = LoggerFactory.getLogger(NumbersProducer.class);
     public static void main(String[] args) throws Exception{
         // create properties object for
         Properties properties = NumbersUtils.loadProperties(Constants.RESOURCES_DIR, "numbers-producer.properties");
@@ -15,7 +18,7 @@ public class NumbersProducer {
         try(Producer<Integer, String> producer = new KafkaProducer<>(properties)) {
             for (int i = 100; i < 1000; i++) {
                 // send data (asynchronously)
-                System.out.println("sending message " + i);
+                logger.info("sending message " + i);
                 producer.send(new ProducerRecord<>(Constants.TOPIC, i, Integer.toString(i)));
                 Thread.sleep(Constants.PRODUCER_DELAY_MS);
             }
